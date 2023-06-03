57a6390c5e0e6dbfb4d92e009fd5cda70909f594
cameronjpr <cameronjpr@gmail.com>
2023-06-03 21:36:01 +0100

    initial commit

---
+++ src/app/Table.tsx
@@ -0,0 +1,24 @@
+export function Table() {
+  return (
+    <ul>
+      <li>Arsenal</li>
+      <li>Aston Villa</li>
+      <li>Brentford</li>
+      <li>Brighton</li>
+      <li>Burnley</li>
+      <li>Chelsea</li>
+      <li>Crystal Palace</li>
+      <li>Everton</li>
+      <li>Leeds United</li>
+      <li>Leicester City</li>
+      <li>Liverpool</li>
+      <li>Manchester City</li>
+      <li>Manchester United</li>
+      <li>Newcastle United</li>
+      <li>Norwich City</li>
+      <li>Southampton</li>
+      <li>Tottenham Hotspur</li>
+      <li>Watford</li>
+    </ul>
+  )
+}
